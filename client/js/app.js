9a406d25800cea1e050e79f452c0e6908d580575
Andrei Onea <andrei.onea@emeal.nttdata.com>
2024-05-18 13:28:50 +0300

    First commit

---
+++ client/js/app.js
@@ -0,0 +1,8 @@
+// Get the button element
+const signUpButton = document.getElementById("signUp");
+
+// Add a click event listener to the button
+signUpButton.addEventListener("click", () => {
+    // Redirect to signUp.html
+    window.location.href = "signUp.html";
+});
---
+++ client/js/signUp.js
@@ -0,0 +1,15 @@
+const signUpForm = document.forms["signUpForm"];
+
+
+
+
+
+
+const signUpInput = document.getElementById("sign_up");
+
+
+
+signUpInput.addEventListener("click", () => {
+    // Redirect to signUp.html
+    window.location.href = "index.html";
+});
